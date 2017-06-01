10981425bfb2420f3dda728e6521a5df89371206
diandi <cnljli@live.com>
2017-06-01 18:33:51 +0800

    add del and move (#4)

--- src/views/canvas/Sprite.vue
+++ src/views/canvas/Sprite.vue
@@ -2,26 +2,33 @@
     <div>
         <ToolsHeader title="Sprite" slogan="合成雪碧图工具"></ToolsHeader>
         <div class="content">
-            <div class="imgs" ref="imgs">
-                <img :src="img" v-for="img in imgs" class="imgs-item">
+            <div class="imgs" ref="imgs" :class="direction">
+                <img @dragstart="dragstart" @drop.prevent="moveImg" @dragover.prevent
+                     :src="img.src" :title="img.title" v-for="(img,idx) in imgs" 
+                     :index="idx"
+                     :key="idx" class="imgs-item" :style="itemsStyle">
             </div>
-            <br/>
-            <div class="add-img">
+            <template v-if="direction === 'horizontal'">
+                <br />
+            </template>
+            <div class="add-img" :class="direction">
                 <input type="file" @change="uploadFile" multiple accept="image/jpeg,image/jpg,image/png" />
             </div>
+            <div class="del-img" :class="direction" @click="tips('把元素拖到此处删除')"  @drop.prevent="delImg" @dragover.prevent></div>
         </div>
         <div class="footer">
             <span class="name">方向:</span><select v-model="direction">
                 <option value="horizontal">水平</option>
                 <option value="vertical">垂直</option>
             </select>
-            <span class="name">间隔:</span><input type="text" v-model="span" />
+            <span class="name">间隔:</span><input type="number" v-model="span" />
             <a href="javascript:;" class="btn-down" @click="compose">下载</a>
         </div>
     </div>
 </template>
 <script>
     import ToolsHeader from 'components/ToolsHeader'
+    import 'units/toBlobPolyfill'
     export default {
       data () {
         return {
@@ -30,38 +37,70 @@
           imgs: []
         }
       },
+      computed: {
+        itemsStyle: function () {
+          let changeStyle = 'margin-bottom'
+          if (this.direction === 'horizontal') {
+            changeStyle = 'margin-right'
+          }
+          const style = {}
+          style[`${changeStyle}`] = `${this.span}px`
+          return style
+        }
+      },
       components: {
         ToolsHeader
       },
       methods: {
+        tips (msg) {
+          alert(msg)
+        },
         uploadFile (event) {
           [].slice.call(event.target.files).forEach(file => {
-            this.imgs.push(URL.createObjectURL(file))
+            this.imgs.push({
+              src: URL.createObjectURL(file),
+              title: file.name
+            })
           })
+        },
+        dragstart (event) {
+          event.dataTransfer.setData('Text', [].slice.call(event.target.parentElement.children).indexOf(event.target))
+        },
+        delImg (event) {
+          const index = parseFloat(event.dataTransfer.getData('Text'))
+          this.imgs.splice(index, 1)
+        },
+        moveImg (event) {
+          const curIndex = [].slice.call(event.target.parentElement.children).indexOf(event.target)
+          const index = parseFloat(event.dataTransfer.getData('Text'))
+          this.imgs.splice(curIndex, 0, ...this.imgs.splice(index, 1))
         },
         compose () {
           const imgs = this.$refs.imgs.querySelectorAll('img')
+          if (imgs.length === 0) return ''
           const canvas = document.createElement('canvas')
           const ctx = canvas.getContext('2d')
+          let downName = 'sprite.png'
           imgs.forEach((img, idx) => {
             if (idx === 0) {
               canvas.width = img.naturalWidth
               canvas.height = img.naturalHeight
               if (this.direction === 'horizontal') {
-                canvas.width = canvas.width * imgs.length
+                canvas.width = canvas.width * imgs.length + (imgs.length - 1) * parseFloat(this.span)
               } else {
-                canvas.height = canvas.height * imgs.length
+                canvas.height = canvas.height * imgs.length + (imgs.length - 1) * parseFloat(this.span)
               }
+              downName = `${img.getAttribute('title').replace(/.\w+$/, downName)}`
             }
             if (this.direction === 'horizontal') {
-              ctx.drawImage(img, idx * img.naturalWidth, 0)
+              ctx.drawImage(img, idx * (img.naturalWidth + parseFloat(this.span)), 0)
             } else {
-              ctx.drawImage(img, 0, idx * img.naturalHeight)
+              ctx.drawImage(img, 0, idx * (img.naturalHeight + parseFloat(this.span)))
             }
           })
           canvas.toBlob(blob => {
             const a = document.createElement('a')
-            a.download = 'sprite.png'
+            a.download = downName
             a.href = URL.createObjectURL(blob)
             a.click()
           }, 'image/png')
@@ -75,32 +114,71 @@
         font-size: 0;
         .imgs {
             display: inline-block;
-            border: #b7b0b0 1px dashed;
+            border: #b7b0b0 1px dotted;
             margin-bottom: 5px;
             &:empty {
                 border: none;
+            }
+            &.horizontal {
+                white-space: nowrap;
+                max-width: 100%;
+                border-bottom: none;
+                overflow-x: auto;
+            }
+            &.vertical {
+                max-width: 100px;
+                box-sizing: content-box;
             }
         }
         .imgs-item {
             max-width: 100px;
             display: inline-block;
+            &:last-child {
+                margin-bottom: 0 !important;
+                margin-right: 0 !important;
+            }
         }
+        .del-img,
         .add-img {
             display: inline-block;
             position: relative;
             width: 50px;
             height: 50px;
-            border: #b7b0b0 1px dashed;
+            border: #b7b0b0 1px dotted;
             cursor: pointer;
+            vertical-align: middle;
+            &.vertical {
+                vertical-align: top;
+                margin-left: 10px;
+            }
+        }
+        .del-img {
+            margin-left: 10px;
+            font-size: 14px;
+            text-align: center;
             &::before,
             &::after {
                 content: '';
                 position: absolute;
                 left: 50%;
                 top: 50%;
-                width: 50px;
-                height: 1px;
-                background: #a09d9d;
+                width: 50%;
+                border-top: #c12b2b 1px solid;
+                transform: translate(-50%, -50%) rotate(45deg);
+            }
+            &::after {
+                transform: translate(-50%, -50%) rotate(135deg);
+            }
+        }
+        .add-img {
+            &::before,
+            &::after {
+                content: '';
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                width: 50%;
+                border-top: #a09d9d 1px solid;
                 transform: translate(-50%, -50%)
             }
             &::after {
